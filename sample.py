4e835ce07db1d30605ecb3a739271dfca1406e30
unknown <mutyalavenkatgopichand@gmail.com>
2021-01-08 15:50:29 +0530

    mysecondcommit

---
+++ sample.py
@@ -0,0 +1,17 @@
+"'welcome to python world'"
+def example():
+    print('welcome to module')
+def add(*a):
+    s=0
+    for i in a:
+        s+=i
+    return s
+def add(*a):
+    return sum(a)
+def mul(a,b):
+    return (a*b)
+def sub(a,b):
+    return (a-b)
+def div(a,b):
+    return (a/b)
+
